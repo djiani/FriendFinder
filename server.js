0d8e10cc55bcf6cea04d518005c0e0929d3b7f05
Uncle Sam <djiasara@gmail.com>
2019-05-16 16:54:45 -0400

    add route to survey

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ app.get('/', function(req, res){
     res.sendFile(path.join(`${__dirname}/app/public/home.html`));
 });
 
-
-
-
+app.get('/survey', function(req, res){
+    res.sendFile(path.join(`${__dirname}/app/public/survey.html`));
+});
 
 
 
