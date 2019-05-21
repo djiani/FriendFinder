310dfde1b2d4e43b7e148b336e9aa347cd5548ed
Uncle Sam <djiasara@gmail.com>
2019-05-21 01:25:29 -0400

    implement post api

--- app/data/friends.js
+++ app/data/friends.js
@@ -25,3 +25,5 @@ const Friends = [
         "scores":[5, 1, 4, 4, 5,1, 2, 5, 4, 1]
       },
 ]
+
+module.exports = Friends;
--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const path = require('path');
+const Friends = require('./app/data/friends');
+
 
 
 const app = express();
 const PORT = 3000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 const htmlRoutes = require('./app/routing/htmlRoutes');
 htmlRoutes(app);
 
+app.get('/api/friends', function(req, res){
+    res.json(Friends);
+})
 
-
+app.post('/api/friends', function(req, res){
+    console.log(req.body);
+    res.end();
+    //return best match friend!
+})
 
 
 app.listen(PORT, function(){
