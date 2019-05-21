04df81975e195316e52496cefd79d90a9f5707d6
Uncle Sam <djiasara@gmail.com>
2019-05-21 03:06:37 -0400

    implement find best match

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,28 +2,19 @@ const Friends = [
     {
         "name":"Ahmed",
         "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[5, 1, 4, 4, 5,1, 2, 5, 4, 1]
+        "scores":[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
       },
       {
         "name":"Ahmed",
         "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[5, 1, 4, 4, 5,1, 2, 5, 4, 1]
+        "scores":[5, 4, 1, 4, 3, 1, 4, 2, 4, 1]
       },
       {
         "name":"Ahmed",
         "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[5, 1, 4, 4, 5,1, 2, 5, 4, 1]
-      },
-      {
-        "name":"Ahmed",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[5, 1, 4, 4, 5,1, 2, 5, 4, 1]
-      },
-      {
-        "name":"Ahmed",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[5, 1, 4, 4, 5,1, 2, 5, 4, 1]
-      },
+        "scores":[3, 1, 4, 4, 3, 1, 2, 4, 4, 2]
+      }
+      
 ]
 
 module.exports = Friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,4 @@
-
+const Friends = require('../data/friends');
 
 function apiRoutes(app){
     app.get('/api/friends', function(req, res){
@@ -7,7 +7,17 @@ function apiRoutes(app){
     
     app.post('/api/friends', function(req, res){
         console.log(req.body);
-        res.end();
+        console.log(Friends);
+        const sumArray = Friends.map(function(data){
+           return  data.scores.reduce(function(sum, val, index){
+               return  sum + Math.abs(parseInt(val)- parseInt(req.body.scores[index]));
+            }, 0);
+        });
+        console.log(sumArray);
+        let indexBestMatch = sumArray.indexOf(Math.min(...sumArray));
+        console.log(indexBestMatch);
+        Friends.push(req.body);
+        res.json(Friends[indexBestMatch]);
         //return best match friend!
     })
 }
