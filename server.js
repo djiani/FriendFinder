405e9497ed6f9ee77fa1ee478b996f26e95b100d
Uncle Sam <djiasara@gmail.com>
2019-05-21 21:25:46 -0400

    app styling

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const path = require('path');
 
-
-
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(express.static( path.join(__dirname, "app/public") ));
 
 const htmlRoutes = require('./app/routing/htmlRoutes');
 htmlRoutes(app);
